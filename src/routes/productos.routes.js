58a039099127ca332a1c771d579e26201eca614c
Juancolque04 <jdiazcolque04@gmail.com>
2025-06-01 23:05:39 -0300

    agregando rutas faltantes para la optimizacion de produtos

--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,34 +1,41 @@
-import { Router } from 'express';
-import { check } from 'express-validator';
-import { 
-    getProductos, 
-    getProductoById, 
-    createProducto, 
-    updateProducto, 
-    deleteProducto,
-    searchProductos
-} from '../controllers/productos/producto.controller.js';
-import { verifyToken } from '../middlewares/verifyToken.js';
+import { Router } from "express"
+import { check } from "express-validator"
+import {
+  getProductos,
+  getProductoById,
+  createProducto,
+  updateProducto,
+  deleteProducto,
+  searchProductos,
+  getProductosPaginados,
+  searchProductosRapido,
+} from "../controllers/productos/producto.controller.js"
+import { verifyToken } from "../middlewares/verifyToken.js"
 
-const router = Router();
+const router = Router()
 
 // Middleware para verificar token en todas las rutas
-router.use(verifyToken());
+router.use(verifyToken())
 
 // Validaciones para crear/actualizar producto
 const validateProducto = [
-    check('codigo').notEmpty().withMessage('El código es obligatorio'),
-    check('nombre').notEmpty().withMessage('El nombre es obligatorio'),
-    check('precio').isNumeric().withMessage('El precio debe ser un número')
-        .custom(value => value > 0).withMessage('El precio debe ser mayor a cero'),
-];
+  check("codigo").notEmpty().withMessage("El código es obligatorio"),
+  check("nombre").notEmpty().withMessage("El nombre es obligatorio"),
+  check("precio")
+    .isNumeric()
+    .withMessage("El precio debe ser un número")
+    .custom((value) => value > 0)
+    .withMessage("El precio debe ser mayor a cero"),
+]
 
 // Rutas
-router.get('/', getProductos);
-router.get('/search', searchProductos);
-router.get('/:id', getProductoById);
-router.post('/', validateProducto, createProducto);
-router.put('/:id', validateProducto, updateProducto);
-router.delete('/:id', deleteProducto);
+router.get("/", getProductos)
+router.get("/paginados", getProductosPaginados)
+router.get("/search-rapido", searchProductosRapido)
+router.get("/search", searchProductos)
+router.get("/:id", getProductoById)
+router.post("/", validateProducto, createProducto)
+router.put("/:id", validateProducto, updateProducto)
+router.delete("/:id", deleteProducto)
 
-export default router;
+export default router
